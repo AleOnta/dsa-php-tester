208c7741e6cf78910a53fa07871283225754374e
AleOnta <casualotn@gmail.com>
2025-01-06 21:48:49 +0100

    initialized Container class for DI in app

---
+++ backend/src/core/Container.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Backend\Core;
+
+class Container
+{
+    private array $services = [];
+
+    public function set(string $serviceKey, callable $resolver)
+    {
+        # store the service inside the container
+        $this->services[$serviceKey] = $resolver;
+    }
+
+    public function get(string $serviceKey)
+    {
+        # check if the service is stored in the container
+        if (!isset($this->services[$serviceKey])) {
+            throw new \Exception("Service [{$serviceKey}] doesn't exists!");
+        }
+        # return the required service
+        return $this->services[$serviceKey];
+    }
+}
