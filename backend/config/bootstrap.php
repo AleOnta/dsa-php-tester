d9e610bfb9e6ae80ba8afc215e957491df7a12f3
Ale Onta <casualotn@gmail.com>
2025-01-12 21:46:07 +0100

    add services registration in DI container during config

--- backend/config/bootstrap.php
+++ backend/config/bootstrap.php
@@ -1,5 +1,9 @@
 <?php
 
+use Backend\Controllers\UserController;
+use Backend\Database\Database;
+use Backend\Repositories\UserRepository;
+use Backend\Services\UserService;
 use Backend\Utils\EnvLoader;
 use Backend\Utils\AppConstants;
 use Backend\Controllers\RootController;
@@ -10,7 +14,15 @@ require_once AppConstants::UTILS_DIR . "helpers.php";
 # Initialize App Container
 $container = new \Backend\Core\Container;
 
-# registering controllers
-$container->set(RootController::class, function() {
-    return new RootController();
-});
+# registering DB connection into DI container
+$container->set('db', fn() => Database::getInstance());
+
+# registering repository classes
+$container->set(UserRepository::class, fn($c) => new UserRepository($c->get('db')));
+
+# registering service classes
+$container->set(UserService::class, fn($c) => new UserService($c->get(UserRepository::class)));
+
+# registering controller classes
+$container->set(RootController::class, fn() => new RootController());
+$container->set(UserController::class, fn($c) => new UserController($c->get(UserService::class)));
