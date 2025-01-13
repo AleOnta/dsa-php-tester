266f50b3cbffacbfcc5c9d1423ae6f8087bb8973
AleOnta <casualotn@gmail.com>
2025-01-13 22:11:41 +0100

    adds Request attr to base controller classs

--- backend/src/controllers/Controller.php
+++ backend/src/controllers/Controller.php
@@ -4,6 +4,13 @@ namespace Backend\Controllers;
 
 class Controller
 {
+    protected \Backend\Core\Request $request;
+
+    public function __construct()
+    {
+        $this->request = new \Backend\Core\Request();
+    }
+
     public function response($code, $responseObj)
     {
         http_response_code($code);
--- backend/src/controllers/UserController.php
+++ backend/src/controllers/UserController.php
@@ -4,15 +4,18 @@ namespace Backend\Controllers;
 
 use Backend\Services\UserService;
 
-class UserController extends Controller {
+class UserController extends Controller
+{
 
     protected UserService $userService;
 
-    public function __construct(UserService $userService) {
+    public function __construct(UserService $userService)
+    {
         $this->userService = $userService;
     }
 
-    public function register() {
-        $this->response(200, ['status' => true, 'message' => 'registering user']);
+    public function register()
+    {
+        dd($this->request);
     }
 }
