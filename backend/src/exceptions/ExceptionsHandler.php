fb1b9a8fe9050dd85c5fcf2f07869eb9f9aff59c
AleOnta <casualotn@gmail.com>
2025-01-19 13:02:26 +0100

    implemented new exceptions in exception handler class

--- backend/src/exceptions/ExceptionsHandler.php
+++ backend/src/exceptions/ExceptionsHandler.php
@@ -2,12 +2,12 @@
 
 namespace Backend\Exceptions;
 
-use Throwable;
+use Exception;
 
 class ExceptionsHandler
 {
 
-    public function handle(Throwable $exception): void
+    public function handle(\Throwable $exception): void
     {
         # format the response to return
         $response = $this->formatResponse($exception);
@@ -19,17 +19,55 @@ class ExceptionsHandler
         echo json_encode($response['body']);
     }
 
-    private function formatResponse(Throwable $exception): array
+    private function formatResponse(\Throwable $exception): array
     {
         return match (true) {
             $exception instanceof \Backend\Exceptions\ValidationException => $this->handleValidationException($exception),
+            $exception instanceof \Backend\Exceptions\MissingParameterException => $this->handleMissingParameterException($exception),
+            $exception instanceof \Backend\Exceptions\InvalidRequestException => $this->handleInvalidRequestException($exception),
+            default => $this->handleDefaultException($exception)
         };
+    }
+
+    private function handleDefaultException(Exception $exception)
+    {
+        return [
+            'status' => 500,
+            'body' => [
+                'error' => true,
+                'message' => $exception->getMessage()
+            ]
+        ];
     }
 
     private function handleValidationException(ValidationException $exception)
     {
         return [
             'status' => 422,
+            'body' => [
+                'error' => true,
+                'message' => $exception->getMessage(),
+                'details' => $exception->getErrors()
+            ]
+        ];
+    }
+
+    private function handleMissingParameterException(MissingParameterException $exception)
+    {
+        return [
+            'status' => 400,
+            'body' => [
+                'error' => true,
+                'message' => $exception->getMessage(),
+                'details' => $exception->getErrors()
+            ]
+        ];
+    }
+
+    private function handleInvalidRequestException(InvalidRequestException $exception)
+    {
+        return [
+            'status' => 400,
             'body' => [
                 'error' => true,
                 'message' => $exception->getMessage(),
