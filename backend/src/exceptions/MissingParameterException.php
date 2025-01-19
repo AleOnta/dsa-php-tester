85e24d9aba32e6c08e909ad32d0e91d3132e340f
AleOnta <casualotn@gmail.com>
2025-01-19 13:00:45 +0100

    adds missing parameter exception class

---
+++ backend/src/exceptions/MissingParameterException.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Backend\Exceptions;
+
+class MissingParameterException extends \Exception
+{
+    private array $errors;
+
+    public function __construct(string $message, array $errors = [])
+    {
+        parent::__construct($message);
+        $this->errors = $errors;
+    }
+
+    public function getErrors()
+    {
+        return $this->errors;
+    }
+}
