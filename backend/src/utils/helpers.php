e96c7dd6e182d26124ca699a2437177eee367f1e
AleOnta <casualotn@gmail.com>
2025-01-19 13:03:34 +0100

    created shut down handler function

--- backend/src/utils/helpers.php
+++ backend/src/utils/helpers.php
@@ -1,7 +1,21 @@
 <?php
 
-function dd($var='no var passed'): void 
+function dd($var = 'no var passed'): void
 {
     var_dump($var);
     die();
+}
+
+function fatal_error_shutdown(): void
+{
+    $lastError = error_get_last();
+    if (isset($lastError)) {
+        http_response_code(500);
+        echo json_encode([
+            'error' => true,
+            'message' => 'Something went wrong',
+            'last_error' => $lastError
+        ]);
+        die();
+    }
 }
