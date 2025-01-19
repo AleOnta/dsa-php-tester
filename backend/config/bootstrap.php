313cb5c4fb1c3e1678a57f87d6cd6388344b8399
AleOnta <casualotn@gmail.com>
2025-01-19 13:01:42 +0100

    adds exception and fatal error handling functions

--- backend/config/bootstrap.php
+++ backend/config/bootstrap.php
@@ -1,15 +1,23 @@
 <?php
 
-use Backend\Controllers\UserController;
 use Backend\Database\Database;
-use Backend\Repositories\UserRepository;
-use Backend\Services\UserService;
-use Backend\Utils\EnvLoader;
 use Backend\Utils\AppConstants;
+use Backend\Services\UserService;
+use Backend\Controllers\UserController;
 use Backend\Controllers\RootController;
+use Backend\Repositories\UserRepository;
+use Backend\Exceptions\ExceptionsHandler;
 
 include dirname(__DIR__) . "/vendor/autoload.php";
 require_once AppConstants::UTILS_DIR . "helpers.php";
+
+# register error function
+ini_set('display_errors', 0);
+register_shutdown_function('fatal_error_shutdown');
+
+# register exception handler
+$exceptionHandler = new ExceptionsHandler();
+set_exception_handler([$exceptionHandler, 'handle']);
 
 # Initialize App Container
 $container = new \Backend\Core\Container;
