c9edff9c04c2b82b584e644b7e133c089e2052d2
AleOnta <casualotn@gmail.com>
2025-01-26 20:58:40 +0100

    minor changes

--- backend/src/utils/helpers.php
+++ backend/src/utils/helpers.php
@@ -6,20 +6,4 @@ function dd($var = 'no var passed'): void
     die();
 }
 
-function fatal_error_shutdown(): void
-{
-    $lastError = error_get_last();
-    if (isset($lastError)) {
-        http_response_code(500);
-        echo json_encode([
-            'error' => true,
-            'message' => 'Something went wrong',
-            'error' => [
-                'error_message' => $lastError['message'],
-                'file' => $lastError['file'],
-                'line' => $lastError['line']
-            ]
-        ]);
-        die();
-    }
-}
+function fatal_error_shutdown(): void {}
