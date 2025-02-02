dbe585db1e5f2b0a61aea9e920215cc69d915bed
AleOnta <casualotn@gmail.com>
2025-02-02 22:39:11 +0100

    adds async_jobs table migration

---
+++ backend/src/database/migrations/20250202121330_create_async_jobs_table.php
@@ -0,0 +1,29 @@
+<?php
+
+class CreateAsyncJobsTable
+{
+
+    public function up(\PDO $db)
+    {
+        # define create table query
+        $query = "CREATE TABLE async_jobs (
+            id SERIAL PRIMARY KEY,
+            file TEXT   ,
+            job_type TEXT NOT NULL,
+            status TEXT NOT NULL,
+            PROGRESS int DEFAULT 0,
+            message TEXT,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        );";
+        # execute the query
+        $db->exec($query);
+    }
+
+    public function down(\PDO $db)
+    {
+        # define delete query
+        $query = "DROP TABLE async_jobs;";
+        $db->exec($query);
+    }
+}
