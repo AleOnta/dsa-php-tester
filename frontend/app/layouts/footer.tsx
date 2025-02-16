b60e18fc9f94cbd712ac1434f73e1e05bb2afe27
AleOnta <casualotn@gmail.com>
2025-02-16 22:06:24 +0100

    re-initialized frontend with simple react app built from scratch

--- frontend/app/layouts/footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-    return (
-        <footer className="footer text-center absolute bottom-0 w-full bg-slate-700 text-slate-300 p-4">
-            <aside>
-                <p>Copyright © {new Date().getFullYear()} - All right reserved by AleOnta</p>
-            </aside>
-        </footer>
-    );
-}
